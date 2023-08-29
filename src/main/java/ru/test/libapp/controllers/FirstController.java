066a7f855a7218204a3083a91a49d3cebfc5289b
alvator92 <alvator@list.ru>
2023-08-29 10:47:14 +0300

    HttpServletRequest request getParam

--- src/main/java/ru/test/libapp/controllers/FirstController.java
+++ src/main/java/ru/test/libapp/controllers/FirstController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surName = request.getParameter("surName  ");
+        System.out.println("Hello : " + name + ", " + surName);
         return "first/hello";
     }
 
