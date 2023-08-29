2ea54b8efe31bac5d4db7a0daaeeac32ecb70d2c
alvator92 <alvator@list.ru>
2023-08-29 19:53:45 +0300

    create new person

--- src/main/java/ru/test/libapp/controllers/PeopleController.java
+++ src/main/java/ru/test/libapp/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.test.libapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.test.libapp.dao.PersonDao;
+import ru.test.libapp.models.Person;
 
 import java.util.List;
 
@@ -31,5 +30,16 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDao.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
     }
 }
