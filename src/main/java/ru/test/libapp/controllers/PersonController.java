aeb93c0ae027bc4e4473f7f506f636da2d99f105
alvator92 <alvator@list.ru>
2023-09-01 17:58:06 +0300

    Add UPDATE,DELETE METHODS. add PersonValidator.java, PersonRowMapper.java add edit.html

--- src/main/java/ru/test/libapp/controllers/PersonController.java
+++ src/main/java/ru/test/libapp/controllers/PersonController.java
@@ -4,29 +4,77 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.test.libapp.dao.PersonDAO;
+import ru.test.libapp.models.Person;
+import ru.test.libapp.util.PersonValidator;
+
+import javax.validation.Valid;
 
 @Component
 @RequestMapping("/people")
 public class PersonController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PersonController(PersonDAO personDAO) {
+    public PersonController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public String index(Model model) {
-        model.addAttribute("people", personDAO.show());
+        model.addAttribute("people", personDAO.index());
         return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
     }
 
     @GetMapping("/new")
-    public String newPerson() {
-        return "people/index";
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+    @PostMapping
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "people/new";
+
+        personDAO.createPerson(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id,
+                       Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        personValidator.validate(person, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
+        personDAO.updatePerson(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.deletePerson(id);
+        return "redirect:/people";
     }
 }
--- src/main/java/ru/test/libapp/dao/PersonDAO.java
+++ src/main/java/ru/test/libapp/dao/PersonDAO.java
@@ -6,9 +6,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import ru.test.libapp.models.Person;
+import ru.test.libapp.models.PersonRowMapper;
 
+import javax.swing.text.html.Option;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -18,9 +21,34 @@ public class PersonDAO {
     @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+
     }
 
-    public List<Person> show() {
-        return jdbcTemplate.query("Select * from person", new BeanPropertyRowMapper<>(Person.class));
+    public List<Person> index() {
+        return jdbcTemplate.query("Select * from person", new PersonRowMapper());
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM PERSON WHERE person_id=?", new Object[]{id},
+                new PersonRowMapper()).stream().findAny().orElse(null);
+    }
+
+    public Optional<Person> show(String name) {
+        return jdbcTemplate.query("SELECT * FROM PERSON WHERE name=?", new Object[]{name},
+                new PersonRowMapper()).stream().findAny();
+    }
+
+    public void createPerson(Person person) {
+        jdbcTemplate.update("INSERT INTO PERSON (name, age) VALUES(?,?)", person.getName(), person.getAge());
+
+    }
+
+    public void updatePerson(int id, Person updatePerson) {
+        jdbcTemplate.update("UPDATE PERSON SET name=?, age=? WHERE person_id=?",
+                updatePerson.getName(), updatePerson.getAge(), updatePerson.getId());
+    }
+
+    public void deletePerson(int id) {
+        jdbcTemplate.update("DELETE FROM PERSON WHERE person_id=?", id);
     }
 }
